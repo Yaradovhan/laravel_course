2f9654be10d7f9d66e1e5317b519f5982580fe26
yaroslav.dovgan <yaroslav.dovgan@lexico-voip.com>
2019-06-21 13:45:58 +0300

    add Login Feature Test

---
+++ tests/Feature/Admin/LoginTestTest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Tests\Feature\Admin;
+
+
+use Tests\TestCase;
+
+class LoginTestTest extends TestCase
+{
+    public function testForm() :void
+    {
+        $response = $this->get('/login');
+
+        $response
+            ->assertStatus(200)
+            ->assertSee('Login');
+    }
+
+    public function testErrors() :void
+    {
+        $response = $this->post('/login',[
+            'email' => '',
+            'password' => ''
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertSessionHasErrors(['email', 'password']);
+    }
+}
