3385a67be923f280e516f268bf5fa329e2b2f60b
yaroslav.dovgan <yaroslav.dovgan@lexico-voip.com>
2019-07-17 16:52:01 +0300

    two factor auth(axios) done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,45 +1,51 @@
 require('./bootstrap');
 
-// $('.region-selector').each(function () {
-//     var block = $(this);
-//     var selected = block.data('selected');
-//     var url = block.data('source');
-//
-//     var buildSelect = function (parent, items) {
-//         var current = items[0];
-//         var select = $('<select class="form-control">');
-//         var group = $('<div class="form-group">');
-//
-//         select.append($('<option value=""></option>'));
-//         group.append(select);
-//         block.append(group);
-//
-//         axios.get(url, {params: {parent: parent}})
-//             .then(function (response) {
-//                 response.data.forEach(function (region) {
-//                     select.append(
-//                         $("<option>")
-//                             .attr('name', 'regions[]')
-//                             .attr('value', region.id)
-//                             .attr('selected', region.id === current)
-//                             .text(region.name)
-//                     );
-//                 });
-//                 if (current) {
-//                     buildSelect(current, items.slice(1))
-//                 }
-//             })
-//             .catch(function (error) {
-//                 console.error(error);
-//             });
-//     };
-//     buildSelect(null, selected);
-// });
+$('.region-selector').each(function () {
+    var block = $(this);
+    var selected = block.data('selected');
+    var url = block.data('source');
 
-$('.ui.dropdown')
-    .dropdown()
-;
+    var buildSelect = function (parent, items) {
+        var current = items[0];
+        var select = $('<select class="form-control">');
+        var group = $('<div class="form-group">');
 
-$('.menu .item')
-    .tab()
-;
+        select.append($('<option value=""></option>'));
+        group.append(select);
+        block.append(group);
+
+        axios.get(url, {params: {parent: parent}})
+            .then(function (response) {
+                response.data.forEach(function (region) {
+                    select.append(
+                        $("<option>")
+                            .attr('name', 'regions[]')
+                            .attr('value', region.id)
+                            .attr('selected', region.id === current)
+                            .text(region.name)
+                    );
+                });
+                if (current) {
+                    buildSelect(current, items.slice(1))
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+    };
+    buildSelect(null, selected);
+});
+
+$('.ui.dropdown').dropdown();
+$('.menu .item').tab();
+$('.ui.checkbox').checkbox()
+    .first().checkbox({
+    onChange: function() {
+        var block = $(this);
+        var url = block.data('source');
+        axios.post(url)
+            .catch(function (error) {
+                console.error(error);
+            });
+    }
+});
--- resources/views/cabinet/profile/home.blade.php
+++ resources/views/cabinet/profile/home.blade.php
@@ -14,33 +14,37 @@
                 {{$user->name}}
                 {{$user->last_name}}</div>
             {{--<div class="description">--}}
-                {{--Kristy is an art director living in New York.--}}
+            {{--Kristy is an art director living in New York.--}}
             {{--</div>--}}
         </div>
         <div class="extra content">
-                <div class="meta">
-                    <i class="mail icon"></i>
-                    <span>{{$user->email}}</span>
-                </div>
-            @if($user->phone)
             <div class="meta">
-                <i class="phone icon"></i>
-                <span>{{$user->phone}}</span>
-                @if(!$user->isPhoneVerified())
-                    <i>(is not verified)</i>
-                    <form action="{{route('cabinet.profile.phone')}}" method="post">
-                        @csrf
-                        <button type="submit" class="mini ui orange button">Verify</button>
-                    </form>
-                @endif
+                <i class="mail icon"></i>
+                <span>{{$user->email}}</span>
             </div>
+            @if($user->phone)
+                <div class="meta">
+                    <i class="phone icon"></i>
+                    <span>{{$user->phone}}</span>
+                    @if(!$user->isPhoneVerified())
+                        <i>(is not verified)</i>
+                        <form action="{{route('cabinet.profile.phone')}}" method="post">
+                            @csrf
+                            <button type="submit" class="mini ui orange button">Verify</button>
+                        </form>
+                    @endif
+                </div>
             @endif
-            <div class="ui checkbox two_factor">
-                <label>Two factor auth</label>
-                <input type="checkbox">
+            <div class="ui toggle checkbox two_factor">
+                <span>Two factor auth</span>
+                <input type="checkbox" data-source="{{route('cabinet.profile.phone.auth')}}"  {{$user->isPhoneAuthEnabled() ? 'checked' : ''}}  >
             </div>
-            <div class="ui right">
-                <a href="{{ route('cabinet.profile.edit') }}" class="ui green button">Edit</a>
+            <div class="extra content">
+                 <span class="right floated">
+                <a href="{{ route('cabinet.profile.edit') }}" class="circular ui icon button">
+                    <i class="icon settings"></i>
+                </a>
+                </span>
             </div>
         </div>
 
@@ -66,99 +70,37 @@
         </tbody>
     </table>
 
-    <table class="ui definition table">
-        <tbody>
-        <tr>
-            <td class="two wide column">Size</td>
-            <td>1" x 2"</td>
-        </tr>
-        <tr>
-            <td>Weight</td>
-            <td>6 ounces</td>
-        </tr>
-        <tr>
-            <td>Color</td>
-            <td>Yellowish</td>
-        </tr>
-        <tr>
-            <td>Odor</td>
-            <td>Not Much Usually</td>
-        </tr>
-        </tbody>
-    </table>
-
-    <table class="ui definition table">
-        <tbody>
-        <tr>
-            <td class="two wide column">Size</td>
-            <td>1" x 2"</td>
-        </tr>
-        <tr>
-            <td>Weight</td>
-            <td>6 ounces</td>
-        </tr>
-        <tr>
-            <td>Color</td>
-            <td>Yellowish</td>
-        </tr>
-        <tr>
-            <td>Odor</td>
-            <td>Not Much Usually</td>
-        </tr>
-        </tbody>
-    </table>
-    <table class="ui definition table">
-        <tbody>
-        <tr>
-            <td class="two wide column">Size</td>
-            <td>1" x 2"</td>
-        </tr>
-        <tr>
-            <td>Weight</td>
-            <td>6 ounces</td>
-        </tr>
-        <tr>
-            <td>Color</td>
-            <td>Yellowish</td>
-        </tr>
-        <tr>
-            <td>Odor</td>
-            <td>Not Much Usually</td>
-        </tr>
-        </tbody>
-    </table>
-
-{{--    <table class="ui celled table">--}}
-{{--        <tbody>--}}
-{{--        <tr>--}}
-{{--            <th>Phone</th>--}}
-{{--            <td>--}}
-{{--                @if($user->phone)--}}
-{{--                    {{$user->phone}}--}}
-                    {{--@if(!$user->isPhoneVerified())--}}
-                        {{--<i>(is not verified)</i><br/>--}}
-                        {{--<form action="{{route('cabinet.profile.phone')}}" method="post">--}}
-                            {{--@csrf--}}
-                            {{--<button type="submit" class="btn btn-success btn-success">Verify</button>--}}
-                        {{--</form>--}}
-                    {{--@endif--}}
-{{--                @endif--}}
-{{--            </td>--}}
-{{--        </tr>--}}
-{{--        @if($user->phone)--}}
-{{--            <tr>--}}
-{{--                <th>Two factor auth</th>--}}
-{{--                <td>--}}
-{{--                    <form action="{{route('cabinet.profile.phone.auth')}}" method="post">--}}
-{{--                        @csrf--}}
-{{--                        @if($user->isPhoneAuthEnabled())--}}
-{{--                            <button class="btn btn-sm btn-success">On</button>--}}
-{{--                        @else--}}
-{{--                            <button class="btn btn-sm btn-danger">Off</button>--}}
-{{--                        @endif--}}
-{{--                    </form>--}}
-{{--                </td>--}}
-{{--            </tr>--}}
-{{--        @endif--}}
-{{--        </tbody>--}}
+    {{--    <table class="ui celled table">--}}
+    {{--        <tbody>--}}
+    {{--        <tr>--}}
+    {{--            <th>Phone</th>--}}
+    {{--            <td>--}}
+    {{--                @if($user->phone)--}}
+    {{--                    {{$user->phone}}--}}
+    {{--@if(!$user->isPhoneVerified())--}}
+    {{--<i>(is not verified)</i><br/>--}}
+    {{--<form action="{{route('cabinet.profile.phone')}}" method="post">--}}
+    {{--@csrf--}}
+    {{--<button type="submit" class="btn btn-success btn-success">Verify</button>--}}
+    {{--</form>--}}
+    {{--@endif--}}
+    {{--                @endif--}}
+    {{--            </td>--}}
+    {{--        </tr>--}}
+    {{--        @if($user->phone)--}}
+    {{--            <tr>--}}
+    {{--                <th>Two factor auth</th>--}}
+    {{--                <td>--}}
+    {{--                    <form action="{{route('cabinet.profile.phone.auth')}}" method="post">--}}
+    {{--                        @csrf--}}
+    {{--                        @if($user->isPhoneAuthEnabled())--}}
+    {{--                            <button class="btn btn-sm btn-success">On</button>--}}
+    {{--                        @else--}}
+    {{--                            <button class="btn btn-sm btn-danger">Off</button>--}}
+    {{--                        @endif--}}
+    {{--                    </form>--}}
+    {{--                </td>--}}
+    {{--            </tr>--}}
+    {{--        @endif--}}
+    {{--        </tbody>--}}
 @endsection
