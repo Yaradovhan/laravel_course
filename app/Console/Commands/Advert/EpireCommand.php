f6d70a58695405736d3f03526af3f2d0ad414af6
Yaroslav <yara.dovhan@gmail.com>
2019-08-05 10:06:32 +0300

    add expire

---
+++ app/Console/Commands/Advert/EpireCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands\Advert;
+
+use App\Entity\Adverts\Advert\Advert;
+use App\UseCases\Adverts\AdvertService;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class EpireCommand extends Command
+{
+    private $service;
+
+    protected $signature = 'advert:expire';
+
+    protected $description = 'Expire adverts';
+
+    public function __construct(AdvertService $service)
+    {
+        parent::__construct();
+        $this->service = $service;
+    }
+
+    public function handle()
+    {
+        $success = true;
+
+        foreach (Advert::active()->where('expired_at', '<', Carbon::now())->cursor() as $advert) {
+            try {
+                $this->service->expire($advert);
+            } catch (\DomainException $e) {
+                $this->error($e->getMessage());
+                $success = false;
+            }
+        }
+
+        return $success;
+    }
+}
