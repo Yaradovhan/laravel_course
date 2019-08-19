da88ef8b61cb28d8516b8050889758ffe5ce9b6e
Yaroslav <yara.dovhan@gmail.com>
2019-08-19 17:00:42 +0300

    need fix elastic to current version

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-08-19 13:46:34.
+    * Generated on 2019-08-19 13:59:53.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.32 on 2019-08-19 13:46:34.
+ * Generated for Laravel 5.8.32 on 2019-08-19 13:59:52.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
